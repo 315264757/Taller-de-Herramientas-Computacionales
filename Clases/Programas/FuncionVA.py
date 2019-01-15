f0e6fd8f0cbddd68d77989f45b7676d298d2d613
315264757 <hectorchaparro@ciencias.unam.mx>
2019-01-15 15:04:25 -0500

    .

--- Clases/Programas/FuncionVA.py
+++ Clases/Programas/FuncionVA.py
@@ -3,3 +3,27 @@ def vAbsoluto(x):
         return(x)
     else:
         return(-x)
+
+def raiz(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    return(b)
+
+print raiz(1.0)
+print raiz(4.0)
+print raiz(9.0)
+print raiz(9.1)
+print raiz(1000000.0)
+
+def raiz1(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    print(b)
