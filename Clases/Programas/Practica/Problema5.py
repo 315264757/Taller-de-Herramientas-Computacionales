98e1b4302472d15c7bae365db37806ebd92c31f8
315264757 <hectorchaparro@ciencias.unam.mx>
2019-01-26 15:25:30 -0500

    actualizacion de la malla

--- Clases/Programas/Practica/Problema5.py
+++ Clases/Programas/Practica/Problema5.py
@@ -1,6 +1,9 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 
+a = 10
+b = 14
+n = 1
 
 def malla(a,b,n):
     X = []
@@ -15,3 +18,16 @@ def malla(a,b,n):
         Y.append(y)
     for A,B in zip(X,Y):
         print '%5.1f %5.1f' % (A,B)
+    maya = [[A,B] for A,B in zip(X,Y)]
+    global maya
+    return maya
+
+def funcion():
+    L = []
+    for i in range(len(maya)):
+        x = maya[i][0]
+        y = maya[i][1]
+        f = x**2/25 - y**2/49
+        L.append(f)
+        print L
+        
